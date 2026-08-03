05885a9ddf85391947a2d7594c4cd9e644a03bb9
tarosjk <tarosjk@gmail.com>
2026-08-03 10:31:43 +0900

    detail.phpの商品詳細データを挿入

--- src/pizza/detail.php
+++ src/pizza/detail.php
@@ -36,26 +36,53 @@ if (isset($db)) {
 var_dump($pizza);
 ?>
 
-<h1 class="text-center h3">Pizza Detail</h1>
+<div class="container">
 
-<?php if ($pizza): ?>
+  <h1 class="text-center h3">Pizza Detail</h1>
 
-<?php else:  ?>
+  <?php if ($pizza): ?>
 
-  <svg xmlns="http://www.w3.org/2000/svg" class="d-none">
-    <symbol id="exclamation-triangle-fill" viewBox="0 0 16 16">
-      <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
-    </symbol>
-  </svg>
-  <div class="alert alert-danger d-flex align-items-center" role="alert">
-    <svg class="bi flex-shrink-0 me-2" role="img" aria-label="Danger:">
-      <use xlink:href="#exclamation-triangle-fill" />
-    </svg>
-    <div>
-      ピザのデータが存在しません。
+    <div class="row justify-content-center">
+      <div class="col-md-6">
+        <div class="card">
+          <?php
+          $pizza_img = '';
+          if (empty($pizza['image'])) {
+            //画像データがない
+            $pizza_img = 'default.png';
+          } else {
+            // 画像データがある場合
+            $pizza_img = $pizza['image'];
+          }
+          ?>
+          <img src="uploads/<?= $pizza_img; ?>" alt="" class="card-img-top">
+          <div class="card-body">
+            <h3 class="h4 card-title"><?= $pizza['pizza_name']; ?></h3>
+            <p class="card-text"><?= $pizza['toppings']; ?></p>
+            <a href="detail.php?id=<?= $pizza['id']; ?>" class="btn btn-primary">詳細</a>
+          </div>
+        </div>
+      </div>
     </div>
-  </div>
 
-<?php endif; ?>
+  <?php else:  ?>
+
+    <svg xmlns="http://www.w3.org/2000/svg" class="d-none">
+      <symbol id="exclamation-triangle-fill" viewBox="0 0 16 16">
+        <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
+      </symbol>
+    </svg>
+    <div class="alert alert-danger d-flex align-items-center" role="alert">
+      <svg class="bi flex-shrink-0 me-2" role="img" aria-label="Danger:">
+        <use xlink:href="#exclamation-triangle-fill" />
+      </svg>
+      <div>
+        ピザのデータが存在しません。
+      </div>
+    </div>
+
+  <?php endif; ?>
+
+</div>
 
 <?php require './footer.php'; ?>
