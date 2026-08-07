695effeac644c38d1e1f99d34cb57e1be5a22e9f
tarosjk <tarosjk@gmail.com>
2026-08-07 12:00:00 +0900

    TOPページのWordPressループ完成

--- src/food-science/wp-content/themes/food-science/functions.php
+++ src/food-science/wp-content/themes/food-science/functions.php
@@ -6,6 +6,11 @@
 add_theme_support('title-tag');
 
 /**
+ * アイキャッチ画像を使用可能にする
+ */
+add_theme_support('post-thumbnails');
+
+/**
  * titleの区切り文字を変更する
  */
 add_filter('document_title_separator', 'my_document_title_separator');
@@ -13,4 +18,12 @@ function my_document_title_separator(string $separator): string
 {
   $separator = '|';
   return $separator;
+}
+
+
+function my_dump(mixed $data): void
+{
+  echo '<pre>';
+  var_dump($data);
+  echo '</pre>';
 }
--- src/food-science/wp-content/themes/food-science/index.php
+++ src/food-science/wp-content/themes/food-science/index.php
@@ -62,15 +62,31 @@
         <div class="cardList cardList-1row">
 
           <?php while (have_posts()): the_post(); ?>
-            <section id="<?php the_ID(); ?>" class="cardList_item">
-              <a href="#" class="card">
-                <div class="card_label"><span class="label label-black">お知らせ</span></div>
+            <section id="post-<?php the_ID(); ?>" <?php post_class('cardList_item'); ?>>
+              <a href="<?php the_permalink(); ?>" class="card">
+
+                <?php
+                $catgories = get_the_category();
+                // my_dump($catgories);
+                if ($catgories):
+                ?>
+                  <div class="card_label">
+                    <?php foreach ($catgories as $category): ?>
+                      <span class="label label-black"><?= $category->name; ?></span>
+                    <?php endforeach; ?>
+                  </div>
+                <?php endif; ?>
+
                 <div class="card_pic">
-                  <img src="<?= get_template_directory_uri(); ?>/assets/img/home/news_img01@2x.png" alt="">
+                  <?php if (has_post_thumbnail()): ?>
+                    <?php the_post_thumbnail('medium'); ?>
+                  <?php else: ?>
+                    <img src="<?= get_template_directory_uri(); ?>/assets/img/common/noimage.png" alt="">
+                  <?php endif; ?>
                 </div>
                 <div class="card_body">
-                  <h2 class="card_title">タイトルが入ります。タイトルが入ります。タイトルが入ります。タイトルが入ります。</h2>
-                  <time datetime="2022-01-25">2022年01月25日更新</time>
+                  <h2 class="card_title"><?php the_title(); ?></h2>
+                  <time datetime="<?php the_time('Y-m-d'); ?>"><?php the_time('Y年m月d日'); ?>更新</time>
                 </div>
               </a>
             </section>
