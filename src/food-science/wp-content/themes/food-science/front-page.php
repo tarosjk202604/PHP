3841fc8b238bbd0cab0f2ac7227aa785bf98b91b
tarosjk <tarosjk@gmail.com>
2026-08-19 12:57:03 +0900

    TOPページの各種リンクの設定

--- src/food-science/wp-content/themes/food-science/front-page.php
+++ src/food-science/wp-content/themes/food-science/front-page.php
@@ -44,7 +44,7 @@
         スパイシーでヘルシーな本場の味をお楽しみ下さい。
       </p>
       <div class="section_btn">
-        <a href="" class="btn btn-more">もっと見る</a>
+        <a href="<?= get_permalink(35); ?>" class="btn btn-more">もっと見る</a>
       </div>
     </div>
   </div>
@@ -55,7 +55,11 @@
     <div class="section_inner">
       <header class="section_header">
         <h2 class="heading heading-primary"><span>最新情報</span>NEWS</h2>
-        <div class="section_headerBtn"><a href="" class="btn btn-more">もっと見る</a></div>
+        <?php
+        $news = get_term_by('slug', 'news', 'category');
+        $news_link = get_term_link($news);
+        ?>
+        <div class="section_headerBtn"><a href="<?= $news_link; ?>" class="btn btn-more">もっと見る</a></div>
       </header>
       <div class="section_body">
 
@@ -93,7 +97,7 @@
         <li class="infoList_item">
           <span class="infoList_prepend">メールでのお問い合わせ</span>
           <div class="infoList_btn">
-            <a href="" class="btn btn-primary">お問い合わせ</a>
+            <a href="<?= home_url('/contact/'); ?>" class="btn btn-primary">お問い合わせ</a>
           </div>
         </li>
       </ul>
@@ -115,7 +119,7 @@
       <div class="section_body">
         <p>〒162-0846 東京都新宿区市谷左内町21-13</p>
         <div class="section_btn">
-          <a href="#" class="btn btn-primary">アクセスはこちら</a>
+          <a href="<?= get_permalink(43); ?>" class="btn btn-primary">アクセスはこちら</a>
         </div>
       </div>
     </div>
